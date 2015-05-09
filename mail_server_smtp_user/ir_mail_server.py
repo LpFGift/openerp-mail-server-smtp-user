73e6a0674057f6000ac9bdd0504ecab9e8d8afc3
buke <wangbuke@gmail.com>
2015-05-09 16:13:19 +0800

    improve

--- mail_server_smtp_user/ir_mail_server.py
+++ mail_server_smtp_user/ir_mail_server.py
@@ -54,7 +54,10 @@ class ir_mail_server(osv.osv):
             if smtp_encryption is None and tools.config.get('smtp_ssl'):
                 smtp_encryption = 'starttls' # STARTTLS is the new meaning of the smtp_ssl flag as of v7.0
         message.replace_header('From', '%s <%s>' % (message['From'], smtp_user))
-        message.replace_header('Return-Path', '%s' % (smtp_user,))
+        if message.has_key('return-path'):
+            message.replace_header('return-path', '%s' % (smtp_user,))
+        else:
+            message.add_header('return-path', '%s' % (smtp_user,))
 
         return super(ir_mail_server, self).send_email(cr, uid, message, mail_server_id=mail_server_id, smtp_server=smtp_server, smtp_port=smtp_port, smtp_user=smtp_user, smtp_password=smtp_password, smtp_encryption=smtp_encryption, smtp_debug=smtp_debug, context=context)
 
